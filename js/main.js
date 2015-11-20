b87bde08b075f4968130722add124dc3fbc770be
Sahil <sahil@unicommerce.com>
2015-11-20 16:58:38 +0530

    added activetab options

--- js/main.js
+++ js/main.js
@@ -11,53 +11,60 @@
 // that are not closed properly.
 
 ;(function($){
-    $.fn.tab2accordion = function() {
+    $.fn.tab2accordion = function(options) {
+
+        // Establish our default settings
+        var settings = $.extend({
+            activeTab : 0
+        }, options);
+
+        return this.each( function() {
 
 
-        // tabbed content
-        $(".tab_content").hide();
-        $(".tab_content:first").show();
-
-        /* if in tab mode */
-        $("ul.tabs li").click(function() {
-
+            //If Tab Mode
             $(".tab_content").hide();
-            var activeTab = $(this).attr("rel");
-            $("#"+activeTab).fadeIn();
+            console.log(settings.activeTab);
+            $(".tab_content:eq(" + settings.activeTab + ")").show();
 
-            $("ul.tabs li").removeClass("active");
-            $(this).addClass("active");
+            //If Tab mode
+            $("ul.tabs li").click(function() {
 
-            $(".tab_drawer_heading").removeClass("d_active");
-            $(".tab_drawer_heading[rel^='"+activeTab+"']").addClass("d_active");
+                $(".tab_content").hide();
+                var activeTab = $(this).attr("rel");
+                $("#"+activeTab).fadeIn();
+
+                $("ul.tabs li").removeClass("active");
+                $(this).addClass("active");
+
+                $(".tab_drawer_heading").removeClass("d_active");
+                $(".tab_drawer_heading[rel^='"+activeTab+"']").addClass("d_active");
+
+            });
+
+
+            //If Accordion Mode
+            $(".tab_drawer_heading").click(function() {
+
+                var d_activeTab = $(this).attr("rel");
+
+
+                if($(this).hasClass("d_active")){
+                    $("#"+d_activeTab).fadeOut();
+                    $(this).removeClass("d_active");
+                }
+                else{
+                    $("#"+d_activeTab).fadeIn();
+                    $(this).addClass("d_active");
+                }
+
+                $("ul.tabs li").removeClass("active");
+                $("ul.tabs li[rel^='"+d_activeTab+"']").addClass("active");
+            });
+            //New changes End
+
+            /* Extra class "tab_last" to add border to right side of last tab */
+            $('ul.tabs li').last().addClass("tab_last");
 
         });
-
-        //New changes
-        /* if in drawer mode */
-        $(".tab_drawer_heading").click(function() {
-
-            var d_activeTab = $(this).attr("rel");
-
-
-            if($(this).hasClass("d_active")){
-                $("#"+d_activeTab).fadeOut();
-                $(this).removeClass("d_active");
-            }
-            else{
-                $("#"+d_activeTab).fadeIn();
-                $(this).addClass("d_active");
-            }
-
-            $("ul.tabs li").removeClass("active");
-            $("ul.tabs li[rel^='"+d_activeTab+"']").addClass("active");
-        });
-        //New changes End
-
-        /* Extra class "tab_last" to add border to right side of last tab */
-        $('ul.tabs li').last().addClass("tab_last");
-
-
     }
-    return this;
 })(jQuery);
